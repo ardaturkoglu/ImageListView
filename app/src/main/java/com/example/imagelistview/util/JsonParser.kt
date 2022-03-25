8a99dfa11fec7c251a23da1d74ec87c87191a7db
Arda <ardaturkoglu2909@gmail.com>
2022-03-25 03:35:23 +0300

    [JsonParser] JsonParser functions refactored.

--- app/src/main/java/com/example/imagelistview/util/JsonParser.kt
+++ app/src/main/java/com/example/imagelistview/util/JsonParser.kt
@@ -1,27 +1,20 @@
 package com.example.imagelistview.util
 
 import android.content.Context
-import com.example.imagelistview.model.ImageDataModel
 import com.example.imagelistview.model.ImageList
 import com.google.gson.Gson
-import org.json.JSONException
-import org.json.JSONObject
 import java.io.IOException
-import java.io.InputStream
 
-class JsonParser {
-    private var jsonString : String? = null
-    fun getJsonDataFromAsset(context: Context, fileName: String) : ImageList {
-        try {
-            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
-        } catch (ioException: IOException) {
-            ioException.printStackTrace()
-            this.jsonString = null
-        }
-        return parseImageList()
+fun getJsonDataFromAsset(context: Context, fileName: String): ImageList {
+     val jsonString: String?  = try {
+        context.assets.open(fileName).bufferedReader().use { it.readText() }
+    } catch (ioException: IOException) {
+        ioException.printStackTrace()
+        null
     }
-
-    private fun parseImageList() : ImageList{
-           return Gson().fromJson(jsonString, ImageList::class.java)
-    }
+    return parseImageList(jsonString)
 }
+
+private fun parseImageList(jsonString:String?): ImageList = Gson().fromJson(jsonString, ImageList::class.java)
+
+
